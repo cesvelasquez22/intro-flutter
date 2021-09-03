941472994f95c59ce419fc8e9cb6f5d7b4a9ee09
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2021-09-03 17:18:29 -0600

    initial commit

---
+++ 03-bool-condicion.dart
@@ -0,0 +1,11 @@
+void main() {
+  bool activado = true;
+  
+  print(activado);
+    
+  if (!activado) {
+    print('El motor está funcionando');
+  } else {
+    print('Está apagado');
+  }
+}
---
+++ 05-map.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Los mapas son conocidos como diccionario de datos
+  String propiedad = 'soltero';
+  
+  Map<String, dynamic> persona = {
+    'nombre': 'Carlos',
+    'edad': 32,
+    'soltero': true
+  };
+  
+  print(persona['nombre']);
+  print(persona['edad']);
+  print(persona[propiedad]);
+  
+  Map<int, String> personas = {
+    1: 'Tony',
+    2: 'Peter',
+    9: 'Strange'
+  };
+  
+  personas.addAll( { 4: 'Banner' } );
+  
+  print(personas);
+  print(personas[2]);
+}
